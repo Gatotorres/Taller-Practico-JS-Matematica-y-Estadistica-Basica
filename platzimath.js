d82847a392de98afe71748eaf50d5b521820f3ad
Gatotorres <sr.nahueltorres@gmail.com>
2024-01-06 01:26:21 +0100

    calculando el promedio y entrando al metodo reduce

--- platzimath.js
+++ platzimath.js
@@ -1,10 +1,49 @@
 function calcularPromedio(lista) {
-    let sumaLista = 0;
-    for(let i = 0; i < lista.length; i ++){
+    let sumaLista = 0; // aca establecemos una lista y la establecemos en 0
+
+    //aca con el ciclo for le establecemos una variable i en cero también
+    //luego que la i es menor al largo del array Lista, esto para que lo recorra
+    //y luego le pedimos que aumente el valor de i en 1 y así sucesivamente 
+    //mientras i sea menor al largo de la lista.
+    for(let i = 0; i < lista.length; i ++){ 
         sumaLista = sumaLista + lista[i];
+        //acá llamamos la variable en 0, y le decimos que es ella misma (0)
+        //más el array Lista en la posición i
         console.log(sumaLista);
+        //aqui simplemente imprimimos sumaLista para que siempre al terminar un ciclo nos diga 
+        //la cuenta por la que va
     }
     const promedio = sumaLista / lista.length;
-    console.log(promedio);
-    return promedio;
+    //ahora declaramos una variable promedio para establecer la logica
+    //donde a suma lista, que simplemente lo que hace es sumar todos los elementos que 
+    //contenga el array introducido.
+    //y dividirlo por el largo de la lista lista.length 
+    //(es parte de la logica de calcular el promedio)
+    
+    console.log(promedio); // aqui imprimimos el promedio en pantalla
+    return promedio; //y con este return la función devuelve como mensaje final el promedio
 }
+
+
+// Agrega estos elementos al inicio de tu archivo JavaScript
+const arrayInput = document.querySelector('#arrayInput');
+const btnCalcPromedio = document.querySelector('#btnCalcPromedio');
+const resultPromedio = document.querySelector('#resultPromedio');
+
+// Agrega un evento click al botón para calcular el promedio
+btnCalcPromedio.addEventListener('click', () => {
+    // Obtiene el array desde el input y lo convierte en un array de números
+    const inputArray = arrayInput.value.split(',').map(Number);
+
+    // Verifica si el input contiene valores numéricos
+    if (inputArray.some(isNaN)) {
+        resultPromedio.innerText = 'Ingresa un array válido';
+        return;
+    }
+
+    // Llama a la función calcularPromedio con el array obtenido
+    const promedio = calcularPromedio(inputArray);
+
+    // Muestra el resultado en el HTML
+    resultPromedio.innerText = `El promedio es: ${promedio}`;
+});
--- playground.js
+++ playground.js
@@ -3,14 +3,28 @@
 //respuesta sacada de los comentarios
 const users = [];
 
+//como ves lo primero que hacemos es declarar una variable
+
 users.push({ id: 123, name: 'usuario1' });
 users.push({ id: 456, name: 'usuario2' });
 
-function solution(users, id) {
+// luego con el metodo del array .push podemos ingresar elementos a dicho array
+
+function solution(users, id) { //esta funcion va a utilizar dos parametros)
     const user = users.find(user => user.id == id);
+    //utilizando el metodo de los array .find podemos recorrerlo
+    //hasta encotrar ...
     console.log(user?.name || false);
-    return;
+    // aqui lo que haremos es imprimir en pantalla dos opciones OR user.name OR false
+    //sucede que utilizamos ? para preguntar si el elemento user contiene un elemento .name 
+    return; // cerramos la funcion
 }
 
 solution(users, 456);
 solution(users, 999);
+
+// aqui mandamos como llamado a la funcion solution dos argumentos
+//users que hace referencia al array anteriormente declarado, y los numeros dentro
+//hacen referencia a la pregunta de user?.name que es imprimido en pantalla
+//que sigue la logica para comprender si gracias al buscadorde array .find 
+//user => user.id == id y en ese caso imprimir user.name OR false
